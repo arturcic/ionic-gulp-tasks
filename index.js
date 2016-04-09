7e63041a988e54b7df019cc269ea277f3be774af
Artur Stolear <Artur.Stolear@technosoft.nl>
2016-04-09 15:42:27 +0300

    removing browserify es2015 task

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var buildBrowserify_es2015 = require('./browserify-es2015');
-var buildBrowserify_typescript = require('./browserify-typescript');
+var buildBrowserify = require('./browserify-typescript');
 var buildSass = require('./sass-build');
 var copyHTML = require('./html-copy');
 var copyFonts = require('./fonts-copy');
@@ -7,8 +6,7 @@ var copyScripts = require('./scripts-copy');
 var webpack = require('./webpack');
 
 module.exports = {
-    buildBrowserify_es2015: buildBrowserify_es2015,
-    buildBrowserify_typescript: buildBrowserify_typescript,
+    buildBrowserify: buildBrowserify,
     buildSass: buildSass,
     copyHTML: copyHTML,
     copyFonts: copyFonts,

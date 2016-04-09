d512b8de43eb1a3df65e4b9fd1870cd6303625cd
Artur Stolear <artur.stolear@technosoft.md>
2016-04-09 15:23:05 +0300

    adding all available tasks to the module

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var fontsCopy = require("./fonts-copy");
+var buildBrowserify_es2015 = require('./browserify-es2015');
+var buildBrowserify_typescript = require('./browserify-typescript');
+var buildSass = require('./sass-build');
+var copyHTML = require('./html-copy');
+var copyFonts = require('./fonts-copy');
+var copyScripts = require('./scripts-copy');
+var webpack = require('./webpack');
 
 module.exports = {
-    fontsCopy: fontsCopy
+    buildBrowserify_es2015: buildBrowserify_es2015,
+    buildBrowserify_typescript: buildBrowserify_typescript,
+    buildSass: buildSass,
+    copyHTML: copyHTML,
+    copyFonts: copyFonts,
+    copyScripts: copyScripts,
+    webpack: webpack,
 }
